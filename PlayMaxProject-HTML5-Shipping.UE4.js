74706048e563d523ce226403c078ef9503ce94bf
OlgaBudd <olia7924331@gmail.com>
2025-07-31 17:19:40 +0300

    Update PlayMaxProject-HTML5-Shipping.UE4.js

--- PlayMaxProject-HTML5-Shipping.UE4.js
+++ PlayMaxProject-HTML5-Shipping.UE4.js
@@ -1167,9 +1167,9 @@ $(document).ready(function() {
 			});
 */
 // Instead as a fallback, download as ArrayBuffer. (TODO: Figure out the bugs with the above, and switch to using that one instead)
-			fetchOrDownloadAndStore(db, Module.locateFile('PlayMaxProject-HTML5-Shipping.data'), 'arraybuffer').then(function(dataArrayBuffer) {
+			fetchOrDownloadAndStore(db, Module.locateFile('https://dl.dropboxusercontent.com/scl/fi/u6yh8zaaik3ixeed67rb1/PlayMaxProject-HTML5-Shipping.data?rlkey=2yk03td0uwoi3tc1uoxhnqmrc&st=y99o2h30&dl=0'), 'arraybuffer').then(function(dataArrayBuffer) {
 				Module['preloadedPackages'] = {};
-				Module['preloadedPackages'][Module.locateFile('PlayMaxProject-HTML5-Shipping.data')] = dataArrayBuffer;
+				Module['preloadedPackages'][Module.locateFile('https://dl.dropboxusercontent.com/scl/fi/u6yh8zaaik3ixeed67rb1/PlayMaxProject-HTML5-Shipping.data?rlkey=2yk03td0uwoi3tc1uoxhnqmrc&st=y99o2h30&dl=0')] = dataArrayBuffer;
 				return dataJsDownload.then(addScriptToDom);
 			});
 
