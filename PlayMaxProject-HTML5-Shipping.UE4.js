88c3d2e3c195dc38ff0f6a0334d535a6e4b65db7
OlgaBudd <olia7924331@gmail.com>
2025-08-01 14:07:37 +0300

    Update PlayMaxProject-HTML5-Shipping.UE4.js

--- PlayMaxProject-HTML5-Shipping.UE4.js
+++ PlayMaxProject-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/OlgaBudd/playmate_test/refs/heads/main/PlayMaxProject-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/OlgaBudd/playmate_test/main/PlayMaxProject-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
